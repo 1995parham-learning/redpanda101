1ed1cad66b44135fcaacd68683e4958f7ed35e67
Parham Alvani <parham.alvani@gmail.com>
2025-03-01 16:11:53 +0000

    feat: update error description in controller

--- internal/infra/http/controller/order.go
+++ internal/infra/http/controller/order.go
@@ -15,7 +15,13 @@ func (c Order) New(ctx fuego.ContextWithBody[request.Order]) (*model.Order, erro
 	o, err := ctx.Body()
 	if err != nil {
 		return nil, fuego.BadRequestError{
-			Err: err,
+			Err:      err,
+			Type:     "",
+			Title:    "Bad Request",
+			Status:   0,
+			Detail:   "failed to parse request body as an order",
+			Instance: "",
+			Errors:   nil,
 		}
 	}
 
@@ -29,7 +35,13 @@ func (c Order) New(ctx fuego.ContextWithBody[request.Order]) (*model.Order, erro
 
 	if err := c.Producer.Produce(ctx.Context(), d); err != nil {
 		return nil, fuego.InternalServerError{
-			Err: err,
+			Err:      err,
+			Type:     "",
+			Title:    "Internal server error",
+			Status:   0,
+			Detail:   "failed to publish order into Kafka",
+			Instance: "",
+			Errors:   nil,
 		}
 	}
 
