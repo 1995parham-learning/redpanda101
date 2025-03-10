f24272dfb35d9f82deeaa4f7145a64266e9d5bb4
Parham Alvani <parham.alvani@gmail.com>
2025-03-10 10:53:26 +0000

    fix: correct lint issue

--- internal/infra/consumer/metric.go
+++ internal/infra/consumer/metric.go
@@ -12,10 +12,19 @@ type Metric struct {
 
 func NewMetric(reg *prometheus.Registry, namespace, serviceName string) *Metric {
 	dit := prometheus.NewHistogram(prometheus.HistogramOpts{
-		Namespace: namespace,
-		Subsystem: serviceName,
-		Name:      "database_insertion_time_seconds",
-		Help:      "time spend for inserting a record into the database in seconds",
+		Namespace:                       namespace,
+		Subsystem:                       serviceName,
+		Name:                            "database_insertion_time_seconds",
+		Help:                            "time spend for inserting a record into the database in seconds",
+		ConstLabels:                     prometheus.Labels{},
+		Buckets:                         prometheus.DefBuckets,
+		NativeHistogramBucketFactor:     0,
+		NativeHistogramZeroThreshold:    0,
+		NativeHistogramMaxBucketNumber:  0,
+		NativeHistogramMinResetDuration: 0,
+		NativeHistogramMaxZeroThreshold: 0,
+		NativeHistogramMaxExemplars:     0,
+		NativeHistogramExemplarTTL:      0,
 	})
 
 	if err := reg.Register(dit); err != nil {
